c84adc94db9dc48d8819bef9280860c81ff12bf8
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-07 17:15:43 +0200

    Addeda dialouge

--- Secondbuild/Program.cs
+++ Secondbuild/Program.cs
@@ -6,7 +6,21 @@ namespace Secondbuild
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! I like to code.");
-        }
+            Console.WriteLine("Hello User. On a scale of 1 to 10, how are you today?");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num <= 5)
+            {
+                Console.WriteLine("Sorry to hear that. Hope it gets better soon.");
+            }
+            else if (num > 10)
+            {
+                Console.WriteLine("Excellent! That's amazing to hear.");
+            }
+            else
+            {
+                Console.WriteLine("That's good to hear!");
+            }
+        } 
+
     }
 }
