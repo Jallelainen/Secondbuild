00b383563090ab49c30fb6e9daa9f219a2855f0f
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-07 10:25:01 +0200

    Add project files.

---
+++ Secondbuild/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Secondbuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
