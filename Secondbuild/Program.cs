8691423842a01b038f3f02f9d0d8b325bd3f4833
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-07 10:27:02 +0200

    New message

--- Secondbuild/Program.cs
+++ Secondbuild/Program.cs
@@ -6,7 +6,7 @@ namespace Secondbuild
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! I like to code.");
         }
     }
 }
